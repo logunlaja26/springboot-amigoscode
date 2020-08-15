8eff32c0e73c26e6c7fb60d2f1b4a1a93d140830
Olayiwola Ogunlaja <ly@Olayiwolas-MBP.cable.rcn.com>
2020-08-15 12:37:40 -0500

    updated modal

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,134 +1,131 @@
-import React, { Component } from 'react';
-import Container from './Container';
-import Footer from './Footer';
-import Modal from './Modal';
-import './App.css';
-import { getAllStudents } from './client';
-import { LoadingOutlined } from '@ant-design/icons';
-//import Modal from '@material-ui/core/Modal';
-import {
-  Table,Avatar,Spin
-} from 'antd';
+import React, { Component } from "react";
+import Container from "./Container";
+import Footer from "./Footer";
+//import Modal from "./Modal";
+import "./App.css";
+import { getAllStudents } from "./client";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Table, Avatar, Spin, Modal } from "antd";
 
-
-
-
-const getIndicatorIcon = () => <LoadingOutlined style={{ fontSize: 24 }} spin />;
+const getIndicatorIcon = () => (
+  <LoadingOutlined style={{ fontSize: 24 }} spin />
+);
 
 class App extends Component {
-
   state = {
     students: [],
     isFetching: false,
-    show : false, 
-   
-    //isAddStudentModalVisible: true
-  }
-
-  componentDidMount () {
-    this.fetchStudents()
-  }
-
-  showModal = () => {
-    this.setState({ show: true });
+    isAddStudentModalVisible: true,
   };
 
-  hideModal = () => {
-    this.setState({ show: false });
-  };
+  componentDidMount() {
+    this.fetchStudents();
+  }
 
-  //openAddStudentModal = () => this.setState({isAddStudentModalVisible: true})
+  // showModal = () => {
+  //   this.setState({ show: true });
+  // };
 
-  //closeAddStudentModal = () => this.setState({isAddStudentModalVisible: false})
+  // hideModal = () => {
+  //   this.setState({ show: false });
+  // };
 
-  
+  openAddStudentModal = () => this.setState({ isAddStudentModalVisible: true });
+
+  closeAddStudentModal = () =>
+    this.setState({ isAddStudentModalVisible: false });
 
   fetchStudents = () => {
     this.setState({
-      isFetching: true
+      isFetching: true,
     });
-    getAllStudents()
-      .then(res => res.json()
-      .then(students => {
-        console.log(students)
-        this.setState( state => ({
-           students,
-          isFetching: false
-        })); 
-    }));
-  }
+    getAllStudents().then((res) =>
+      res.json().then((students) => {
+        console.log(students);
+        this.setState((state) => ({
+          students,
+          isFetching: false,
+        }));
+      })
+    );
+  };
   render() {
-
-    
-    const { students, isFetching } = this.state;
+    const { students, isFetching, isAddStudentModalVisible } = this.state;
 
     if (isFetching) {
       return (
         <Container>
-          <Spin indicator={getIndicatorIcon()}/>
+          <Spin indicator={getIndicatorIcon()} />
         </Container>
       );
     }
 
-    if(students && students.length){
-
+    if (students && students.length) {
       const columns = [
         {
-          title: '',
-          key: 'avatar',
+          title: "",
+          key: "avatar",
           render: (text, student) => (
-            <Avatar size='large'>
-              {`${student.firstName.charAt(0).toUpperCase()}${student.lastName.charAt(0).toUpperCase()}`}
+            <Avatar size="large">
+              {`${student.firstName
+                .charAt(0)
+                .toUpperCase()}${student.lastName.charAt(0).toUpperCase()}`}
             </Avatar>
-          )
+          ),
         },
         {
-          title: 'Student Id',
-          dataIndex: 'studentID',
-          key: 'studentID'
+          title: "Student Id",
+          dataIndex: "studentID",
+          key: "studentID",
         },
         {
-          title: 'First Name',
-          dataIndex: 'firstName',
-          key: 'firstName'
+          title: "First Name",
+          dataIndex: "firstName",
+          key: "firstName",
         },
         {
-          title: 'Last Name',
-          dataIndex: 'lastName',
-          key: 'lastName'
+          title: "Last Name",
+          dataIndex: "lastName",
+          key: "lastName",
         },
         {
-          title: 'Email',
-          dataIndex: 'email',
-          key: 'email'
+          title: "Email",
+          dataIndex: "email",
+          key: "email",
         },
         {
-          title: 'Gender',
-          dataIndex: 'gender',
-          key: 'gender'
-        }
+          title: "Gender",
+          dataIndex: "gender",
+          key: "gender",
+        },
       ];
 
       return (
-           <Container>
-            <Table 
-              dataSource={students} 
-              columns={columns} 
-              pagination={false}
-              rowKey='studentID'/>
-              <Modal show={this.state.show} handleClose={this.hideModal}>
-          <p>Modal</p>
-          <p>Data</p>
-        </Modal>
-            <Footer 
-              numberOfStudents={students.length}
-              handleAddStudentClickEvent={this.showModal}/>
-          </Container>
-       
-        );
+        <Container>
+          <Table
+            dataSource={students}
+            columns={columns}
+            pagination={false}
+            rowKey="studentID"
+          />
+          <Modal
+            title="Add new student"
+            visible={this.isAddStudentModalVisible}
+            onOK={this.closeAddStudentModal}
+            onCancel={this.state}
+            width={1000}
+          >
+            <h1>Antd Hello</h1>
+          </Modal>
+          <Footer
+            numberOfStudents={students.length}
+            handleAddStudentClickEvent={this.openAddStudentModal}
+          />
+        </Container>
+      );
     }
-    
-    return <h1>No students found</h1>
+
+    return <h1>No students found</h1>;
   }
 }
 
--- src/js/src/Footer.js
+++ src/js/src/Footer.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import Container from './Container';
-import { Avatar } from 'antd';
-import Button from '@material-ui/core/Button';
-import './Footer.css';
+import React from "react";
+import Container from "./Container";
+import { Avatar, Button } from "antd";
+//import Button from '@material-ui/core/Button';
+import "./Footer.css";
 
 const Footer = (props) => (
-    <div className='footer'>
-        <Container>
-            {props.numberOfStudents ? 
-                <Avatar 
-                style={{backgroundColor: '#f56a00',marginRight: '5px'}}
-                size='large'>{props.numberOfStudents}</Avatar> : null
-            }
-            <Button variant="contained" onClick={handleAddStudentClickEvent()} color="primary"> Add new student +</Button>
-        </Container>
-    </div>
+  <div className="footer">
+    <Container>
+      {props.numberOfStudents ? (
+        <Avatar
+          style={{ backgroundColor: "#f56a00", marginRight: "5px" }}
+          size="large"
+        >
+          {props.numberOfStudents}
+        </Avatar>
+      ) : null}
+      <Button
+        onClick={() => {
+          console.log("button click");
+        }}
+        color="primary"
+      >
+        {" "}
+        Add new student +
+      </Button>
+    </Container>
+  </div>
 );
 
 export default Footer;
